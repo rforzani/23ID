8618947c3a4d745de72bb26728808fb7691b6161
Riccardo Forzani <rforzani@Mac.lan>
2025-02-01 14:19:46 +0100

    Completed Draft  Digital Identity Smart contract

--- contracts/DigitalIdentity.sol
+++ contracts/DigitalIdentity.sol
@@ -287,13 +287,312 @@ contract DigitalIdentityNFT is ERC721, AccessControl {
     }
 
     // ------------------------------------------------------------------------
+    // WEB3 FUNCTIONS
+    // ------------------------------------------------------------------------
+
+    /**
+     * @dev Link an external wallet address to the identity.
+     * @param tokenId The token ID of the user's identity NFT.
+     * @param wallet The external wallet address to link.
+     */
+    function linkWallet(uint256 tokenId, address wallet) external onlyRole(AI_AGENT_ROLE) {
+        require(wallet != address(0), "Cannot link zero address");
+        _identityData[tokenId].linkedWallets.push(wallet);
+        _addressToTokenMap[wallet] = tokenId;
+
+        emit IdentityDataUpdated(tokenId, "linkedWallets", wallet.toHexString());
+    }
+
+    /**
+     * @dev Link a registered Web3 platform to the user's identity.
+     *      Checks if the platform is registered in the PlatformRegistry.
+     * @param tokenId The token ID of the user's identity NFT.
+     * @param platformAddr The address of the Web3 platform to link.
+     */
+    function linkWeb3Platform(uint256 tokenId, address platformAddr) external {
+        require(_addressToTokenMap[msg.sender] == tokenId, "Not allowed to link platform");
+        require(platformRegistry.isPlatformRegistered(platformAddr), "Platform not registered");
+        _identityData[tokenId].registeredWeb3Platforms.push(platformAddr);
+
+        emit IdentityDataUpdated(tokenId, "registeredWeb3Platforms", platformAddr.toHexString());
+    }
+
+    /**
+     * @dev Upvote a user's post (referenced by IPFS hash on a given platform).
+     *      This increments their totalUpvotes counter and stores the reference.
+     * @param tokenId The token ID of the user's identity NFT.
+     * @param platform The address of the platform where the post exists.
+     * @param ipfsHash The IPFS hash of the post content.
+     */
+    function upvotePost(uint256 tokenId, address platform, string calldata ipfsHash) external {
+        _requireMinted(tokenId);
+        require(platformRegistry.isPlatformRegistered(platform), "Platform not registered");
+
+        IdentityData storage data = _identityData[tokenId];
+
+        // Ensure the platform is linked to the user's identity
+        bool isLinked = false;
+        uint256 platformsLength = data.registeredWeb3Platforms.length;
+        for (uint256 i = 0; i < platformsLength; ) {
+            if (data.registeredWeb3Platforms[i] == platform) {
+                isLinked = true;
+                break;
+            }
+            unchecked { ++i; }
+        }
+        require(isLinked, "Platform not linked to identity");
+
+        data.totalUpvotes += 1;
+
+        // Store the post reference if it doesn't already exist
+        bool exists = false;
+        uint256 upvotedLength = data.upvotedPosts.length;
+        for (uint256 i = 0; i < upvotedLength; ) {
+            if (keccak256(bytes(data.upvotedPosts[i].ipfsHash)) == keccak256(bytes(ipfsHash))) {
+                exists = true;
+                break;
+            }
+            unchecked { ++i; }
+        }
+        if (!exists) {
+            data.upvotedPosts.push(PostReference({platform: platform, ipfsHash: ipfsHash}));
+        }
+
+        emit PostUpvoted(tokenId, msg.sender, ipfsHash);
+    }
+
+    /**
+     * @dev Request a downvote for a user's post (i.e., report it).
+     *      The post is moved into a pending queue, to be reviewed by the AI agent.
+     * @param tokenId The token ID of the user's identity NFT.
+     * @param platform The address of the platform where the post exists.
+     * @param ipfsHash The IPFS hash of the post content.
+     */
+    function requestDownvote(uint256 tokenId, address platform, string calldata ipfsHash) external {
+        _requireMinted(tokenId);
+        require(platformRegistry.isPlatformRegistered(platform), "Platform not registered");
+
+        IdentityData storage data = _identityData[tokenId];
+
+        // Ensure the platform is linked to the user's identity
+        bool isLinked = false;
+        uint256 platformsLength = data.registeredWeb3Platforms.length;
+        for (uint256 i = 0; i < platformsLength; ) {
+            if (data.registeredWeb3Platforms[i] == platform) {
+                isLinked = true;
+                break;
+            }
+            unchecked { ++i; }
+        }
+        require(isLinked, "Platform not linked to identity");
+
+        // Add the post to the pendingDownvotePosts array if it doesn't already exist
+        bool exists = false;
+        uint256 pendingLength = data.pendingDownvotePosts.length;
+        for (uint256 i = 0; i < pendingLength; ) {
+            if (keccak256(bytes(data.pendingDownvotePosts[i].ipfsHash)) == keccak256(bytes(ipfsHash))) {
+                exists = true;
+                break;
+            }
+            unchecked { ++i; }
+        }
+        if (!exists) {
+            data.pendingDownvotePosts.push(PostReference({platform: platform, ipfsHash: ipfsHash}));
+        }
+
+        emit PostPendingDownvote(tokenId, msg.sender, ipfsHash);
+    }
+
+    // ------------------------------------------------------------------------
+    // AI AGENT FUNCTIONS
+    // ------------------------------------------------------------------------
+
+    /**
+     * @dev The AI agent reviews the post. If it violates guidelines, a downvote is finalized.
+     *      Removes the post from pendingDownvotePosts and increments totalDownvotes if violation is true.
+     * @param tokenId The token ID of the user's identity NFT.
+     * @param pendingIndex The index of the reported post in pendingDownvotePosts.
+     * @param violation True if the post violates guidelines, false otherwise.
+     */
+    function finalizeDownvote(
+        uint256 tokenId,
+        uint256 pendingIndex,
+        bool violation
+    ) external onlyRole(AI_AGENT_ROLE) {
+        _requireMinted(tokenId);
+
+        IdentityData storage data = _identityData[tokenId];
+        require(pendingIndex < data.pendingDownvotePosts.length, "Invalid index for pending post");
+
+        // Grab the reported post reference
+        PostReference memory postRef = data.pendingDownvotePosts[pendingIndex];
+
+        // If there's a violation, increment downvotes and store it in downvotedPosts
+        if (violation) {
+            data.totalDownvotes += 1;
+            data.downvotedPosts.push(postRef);
+
+            emit PostDownvoted(tokenId, msg.sender, postRef.ipfsHash);
+        }
+
+        // Remove the post from the pending list using a "swap and pop" to keep array efficient
+        uint256 lastIndex = data.pendingDownvotePosts.length - 1;
+        if (pendingIndex < lastIndex) {
+            data.pendingDownvotePosts[pendingIndex] = data.pendingDownvotePosts[lastIndex];
+        }
+        data.pendingDownvotePosts.pop();
+    }
+
+    // ------------------------------------------------------------------------
+    // VIEW FUNCTIONS
+    // ------------------------------------------------------------------------ù
+
+    /**
+     * @dev Returns the token ID of the user's identity NFT.
+     * @param user The address of the user.
+     * @return tokenId The token ID of the user's identity NFT.
+     */
+    function getTokenId(address user) external view returns (uint256 tokenId) {
+        tokenId = _addressToTokenMap[user];
+    }
+
+    /**
+     * @dev Returns the total number of upvotes and downvotes for the given tokenId.
+     * @param tokenId The token ID of the user's identity NFT.
+     * @return upvotes The total number of upvotes.
+     * @return downvotes The total number of downvotes.
+     */
+    function getReputationCounters(uint256 tokenId)
+        external
+        view
+        returns (uint256 upvotes, uint256 downvotes)
+    {
+        _requireMinted(tokenId);
+        upvotes = _identityData[tokenId].totalUpvotes;
+        downvotes = _identityData[tokenId].totalDownvotes;
+    }
+
+    /**
+     * @dev Returns the posted references awaiting downvote moderation for a user.
+     * @param tokenId The token ID of the user's identity NFT.
+     * @return pendingPosts An array of PostReference structs awaiting moderation.
+     */
+    function getPendingDownvotePosts(uint256 tokenId)
+        external
+        view
+        returns (PostReference[] memory pendingPosts)
+    {
+        _requireMinted(tokenId);
+        pendingPosts = _identityData[tokenId].pendingDownvotePosts;
+    }
+
+    /**
+     * @dev Returns the list of references that have been downvoted after moderation.
+     * @param tokenId The token ID of the user's identity NFT.
+     * @return downvotedPosts An array of PostReference structs that have been downvoted.
+     */
+    function getDownvotedPosts(uint256 tokenId)
+        external
+        view
+        returns (PostReference[] memory downvotedPosts)
+    {
+        _requireMinted(tokenId);
+        downvotedPosts = _identityData[tokenId].downvotedPosts;
+    }
+
+    /**
+     * @dev Returns the user's linked Web3 platforms.
+     * @param tokenId The token ID of the user's identity NFT.
+     * @return platforms An array of platform addresses linked to the user's identity.
+     */
+    function getRegisteredWeb3Platforms(uint256 tokenId)
+        external
+        view
+        returns (address[] memory platforms)
+    {
+        _requireMinted(tokenId);
+        platforms = _identityData[tokenId].registeredWeb3Platforms;
+    }
+
+    /**
+     * @dev Retrieves the social follower count for a specific platform.
+     * @param tokenId The token ID of the user's identity NFT.
+     * @param platform The name of the social media platform.
+     * @return followerCount The number of followers the user has on the specified platform.
+     */
+    function getSocialFollowers(uint256 tokenId, string calldata platform)
+        external
+        view
+        returns (uint256 followerCount)
+    {
+        _requireMinted(tokenId);
+        followerCount = _identityData[tokenId].socialFollowers[platform];
+    }
+
+    /**
+     * @dev Retrieves all verified interests/skills for a given tokenId.
+     * @param tokenId The token ID of the user's identity NFT.
+     * @return interests An array of verified interests or skills.
+     */
+    function getVerifiedInterests(uint256 tokenId)
+        external
+        view
+        returns (string[] memory interests)
+    {
+        _requireMinted(tokenId);
+        interests = _identityData[tokenId].verifiedInterests;
+    }
+
+    /**
+     * @dev Retrieves all job positions for a given tokenId.
+     * @param tokenId The token ID of the user's identity NFT.
+     * @return positions An array of job positions or roles.
+     */
+    function getJobPositions(uint256 tokenId)
+        external
+        view
+        returns (string[] memory positions)
+    {
+        _requireMinted(tokenId);
+        positions = _identityData[tokenId].jobPositions;
+    }
+
+    /**
+     * @dev Retrieves all linked wallet addresses for a given tokenId.
+     * @param tokenId The token ID of the user's identity NFT.
+     * @return wallets An array of linked wallet addresses.
+     */
+    function getLinkedWallets(uint256 tokenId)
+        external
+        view
+        returns (address[] memory wallets)
+    {
+        _requireMinted(tokenId);
+        wallets = _identityData[tokenId].linkedWallets;
+    }
+
+    /**
+     * @dev Retrieves all upvoted posts for a given tokenId.
+     * @param tokenId The token ID of the user's identity NFT.
+     * @return upvotedPosts An array of PostReference structs representing upvoted posts.
+     */
+    function getUpvotedPosts(uint256 tokenId)
+        external
+        view
+        returns (PostReference[] memory upvotedPosts)
+    {
+        _requireMinted(tokenId);
+        upvotedPosts = _identityData[tokenId].upvotedPosts;
+    }
+
+    // ------------------------------------------------------------------------
     // INTERNAL / PRIVATE UTILS
     // ------------------------------------------------------------------------
 
     /**
-    * @dev Internal function to revert if the token does not exist.
-    * @param tokenId The token ID to check.
-    */
+     * @dev Internal function to revert if the token does not exist.
+     * @param tokenId The token ID to check.
+     */
     function _requireMinted(uint256 tokenId) internal view {
         require(_ownerOf(tokenId) != address(0), "Invalid tokenId");
     }
